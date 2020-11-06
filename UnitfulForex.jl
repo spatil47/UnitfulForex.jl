9a49b6bc3935afd738dd7cc6919d2ff88003aba7
Siddharth Patil <sid@sidpatil.com>
2020-11-06 13:42:06 -0500

    Started with basic attempt.
    This isn't in package form yet.

---
+++ UnitfulForex.jl
@@ -0,0 +1,27 @@
+module ForexUnits
+using HTTP, JSON
+using Unitful
+
+function request(url)
+    try
+        response = HTTP.get(url)
+        return String(response.body)
+    catch e
+        return "E: $e"
+    end
+end
+
+@dimension ¤ "¤" Currency
+@refunit EUR "EUR" Euro ¤ true
+
+rates = (JSON.parse ∘ request)("https://api.exchangeratesapi.io/latest")["rates"]
+
+for rate ∈ rates
+    @eval @unit $(rate[1]) "$(rate[1])" $(rate[1]) (1/$(rate[2]))*EUR true
+end
+
+end
+
+using Unitful
+
+Unitful.register(ForexUnits)
